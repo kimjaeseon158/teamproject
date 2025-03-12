cbabbb4c1e284c9d85e056d9d62d7a3973a6dd91
rlawotjs158 <rlawotjs158@naver.com>
2025-03-12 21:11:26 +0900

    front-1

---
+++ teamproject/front/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(){
+  return (
+    <div class="loginBK">
+      <div class="checkpoint">
+        <button class="Administrator">관리자</button>
+        <button class="Staff">사원</button>
+      </div>
+      <div class="loginBox">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
