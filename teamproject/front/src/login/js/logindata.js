3bb31f44e65a0d46e2986688c6508ccf9178f5e8
rlawotjs158 <rlawotjs158@naver.com>
2025-03-12 22:58:03 +0900

    update

--- teamproject/front/src/login/js/logindata.js
+++ teamproject/front/src/login/js/logindata.js
@@ -11,11 +11,6 @@ export const HandleLogin = async (id, password, otp, role) => {
         });
         console.log(response.data.id);
     } catch (error) {
-        if (error.response) {
-            alert(error.response.data.message); // 서버에서 보낸 메시지
-        } else {
-            alert(`서버와 통신 중 오류가 발생했습니다`);
-        }
         console.error("서버 통신 오류:", error);
     }
 };
