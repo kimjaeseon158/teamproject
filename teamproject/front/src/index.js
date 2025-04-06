d3b1417f66164df9409005994abc95693f13657c
rlawotjs158 <rlawotjs158@naver.com>
2025-04-06 22:30:00 +0900

    Front&Back  start

--- teamproject/front/src/index.js
+++ teamproject/front/src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Login from './login/login';
+import App from './App';
 import Calendar from './calenderTest/calender';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Login />
-    {/* <Calendar/> */}
-  </React.StrictMode>
+  <BrowserRouter basename="/">
+    <Routes>
+      <Route path="/" element={<App/>}></Route>
+      {/* <Route path="/" element={<Login />} /> */}
+      <Route path="/data" element={<Calendar />} />
+    </Routes>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
